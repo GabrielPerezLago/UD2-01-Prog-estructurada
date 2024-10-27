254c37f31ea331729f9e47f08aba20f6cfd70a6e
No Administrador <gabrielperezlago.dev@gmail.com>
2024-10-27 03:25:04 +0100

    Commit05

--- views/calculoNotas.gabriel.view.php
+++ views/calculoNotas.gabriel.view.php
@@ -25,3 +25,86 @@
             </div>
         </div>
     </div>
+    <?php if ($data['formSent'] && empty($data['errors'])): ?>
+        <div class="mt-5">
+            <h3 class="text-center">Resultados de Calificaciones por Asignatura</h3>
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped mt-3">
+                    <thead>
+                    <tr>
+                        <th>Asignatura</th>
+                        <th>Media</th>
+                        <th>Aprobados</th>
+                        <th>Suspensos</th>
+                        <th>Nota Máxima</th>
+                        <th>Nota Mínima</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php foreach ($data[S_POST]['resultado'] as $resultado): ?>
+                        <tr>
+                            <td><?php echo $resultado['modulo']; ?></td>
+                            <td><?php echo $resultado['media']; ?></td>
+                            <td><?php echo $resultado['aprobados']; ?></td>
+                            <td><?php echo $resultado['suspensos']; ?></td>
+                            <td><?php echo $resultado['max']; ?></td>
+                            <td><?php echo $resultado['min']; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+
+            <h3 class="text-center mt-5">Listados de Alumnos</h3>
+            <div class="row mt-3">
+                <!-- Alumnos que aprobaron todas las asignaturas -->
+                <div class="col-12 col-lg-6 mb-3">
+                    <div class="alert alert-success">
+                        <h5>Alumnos que han aprobado todo</h5>
+                        <ul>
+                            <?php foreach ($data['alumnosAprobados'] as $alumno): ?>
+                                <li><?php echo $alumno; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Alumnos que suspendieron al menos una asignatura -->
+                <div class="col-12 col-lg-6 mb-3">
+                    <div class="alert alert-warning">
+                        <h5>Alumnos que han suspendido al menos una asignatura</h5>
+                        <ul>
+                            <?php foreach ($data['alumnosSuspensos'] as $alumno): ?>
+                                <li><?php echo $alumno; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Alumnos que promocionan -->
+                <div class="col-12 col-lg-6 mb-3">
+                    <div class="alert alert-info">
+                        <h5>Alumnos que promocionan (máximo un suspenso)</h5>
+                        <ul>
+                            <?php foreach ($data['alumnosPromocionan'] as $alumno): ?>
+                                <li><?php echo $alumno; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- Alumnos que no promocionan -->
+                <div class="col-12 col-lg-6 mb-3">
+                    <div class="alert alert-danger">
+                        <h5>Alumnos que no promocionan (dos o más suspensos)</h5>
+                        <ul>
+                            <?php foreach ($data['alumnosNoPromocionan'] as $alumno): ?>
+                                <li><?php echo $alumno; ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endif; ?>
+</div>
